7dd57ee0dcb954a6babe00831ec61138d8677682
Brad Rosales <73218792@continental.edu.pe>
2023-08-30 21:33:21 -0500

    agregando rutas -> developer

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,12 @@ Route::get('gestion-curricular', function() {
     return "Bienvenido a la Gestión de Curricular";
 });
 
+//Agregando nueva ruta para cargar el plan de evaluacion curricular
+Route::get('gestion-curricular/cargar-plan-evaluacion-curricular', function() {
+    return "En esta página podrá cargar el plan de evaluacion curricular.";
+});
+
+//Agregando nueva ruta para evaluar el Diseño Curricular
+Route::get('gestion-curricular/evaluar-diseño-curricular', function() {
+    return "En esta página podrá evaluar el Diseño Curricular.";
+});
